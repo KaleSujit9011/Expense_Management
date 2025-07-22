0e367f3d88bbf72527b6019a2e1c9706edcb70c0
Sujit_Kale <kalesujit9011@gmail.com>
2025-07-22 14:52:21 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -6,9 +6,9 @@ from add_update import add_update_tab
 from analytics_by_categories import analytics_by_category_tab
 from analytics_by_month import analytics_by_month_tab
 
-API_URL ="https://expensemanagement.streamlit.app/"
-st.title("Expense tracking System")
-tab1,tab2,tab3 = st.tabs(["Add/Update","Analysis by categories","Analysis by month"])
+API_URL = "https://expensemanagement.streamlit.app/"
+st.title("Expense Tracking System")
+tab1,tab2,tab3 = st.tabs(["Add and Update","Analysis by categories","Analysis by month"])
 
 
 with tab1:
